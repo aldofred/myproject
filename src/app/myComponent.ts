154c49bffae2bd2cb51f5078a351a092e9707c69
dev-aevo <dev-aevo@ng-stars.com>
2020-05-22 10:32:56 +0000

    Ajout de mon composant

---
+++ src/app/myComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({ 
+    selector: 'myComponent', 
+    template: '<h1>Bonjour c\'est Aldo </h1>'
+    //: [myComponent.css]
+   })
+
+   export class myComponent { 
+    nom: string;
+   }
